a587a5649e5f3fb492ac170c7693a1d0cd4f7107
asales <augustcezar@gmail.com>
2021-12-20 02:56:01 -0300

    server.js corrigido

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8080
 
-app.use(express.static('./dist/angular-heroku'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
-
-app.listen(process.env.PORT || 8080);
+express()
+  .use(express.static('./dist/angular-heroku'))
+  .get('/*', (req, res) => res.sendFile('dist/angular-heroku/index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
